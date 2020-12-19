2865a19c80e5d8f7346f61f27a622d68ed46e674
Nicole Carlson <70167253+NicoleCarlson123@users.noreply.github.com>
2020-12-18 19:52:28 -0800

    Search bar/ search from front end and backend

--- application/config/database.js
+++ application/config/database.js
@@ -1,15 +1,16 @@
 const mysql =require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit: 50,
+    connectionLimit:50,
     host: 'localhost',
     user: 'root',
     password: 'Busterc@1',
     database: 'csc317db',
     connectionLimit:50,
+    waitForConnections: true,
     debug: false,
 });
 
+const promisePool =pool.promise();
 
-
-module.exports = pool;
+module.exports = promisePool;
--- application/routes/index.js
+++ application/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var isLoggedIn = require('../middleware/routeprotectors').userIsLoggedIn;
+var getRecentPosts = require('../middleware/postsmiddleware').getRecentPosts;
+var db = require("../config/database");
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', getRecentPosts, function(req, res, next) {
   res.render('index');
 });
 
@@ -14,14 +16,38 @@ router.get('/registration',(req, res, next) => {
   res.render("registration");
 });
 
-router.get('/postimage',(req, res, next) => {
-  res.render("postimage");
-});
 router.get('/Imagepost',(req, res, next) => {
   res.render("Imagepost");
 });
+
 router.get('/home',(req, res, next) => {
   res.render("home");
+});
+
+router.use('path', isLoggedIn);
+router.get('/postimage',(req, res, next) => {
+  res.render("postimage");
+});
+
+router.get('/post/:id', (req,res,next)=>{
+ let baseSQL = "SELECT u.username, p.title, p.description, p.photopath, p.created \
+ FROM users u \
+ JOIN posts p \
+ ON u.id=fk_userid \
+ WHERE p.id=?;";
+
+ let postId = req.params.id;
+  db.execute(baseSQL, [postId])
+  .then(([results, fields]) =>{
+    if(results && results.length){
+      let post =results[0];
+      res.render('imagepost', {currentPost:post});
+    }else{
+      req.flash('error', 'This is not the post you are looking for!');
+      res.redirect('/');
+    }
+  })
+
 });
 
 
